72b29d6319855b9944f80bdd520af0adfa6e23e6
davidszegedi <dszegedi@redhat.com>
2024-09-27 16:32:42 +0300

    init repo

---
+++ quarkus-petclinic/src/main/java/org/acme/rest/WelcomeResource.java
@@ -0,0 +1,25 @@
+package org.acme.rest;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import io.quarkus.qute.Template;
+import io.quarkus.qute.TemplateInstance;
+
+@Path("/")
+public class WelcomeResource {
+
+    // TODO: Inject template
+    @Inject
+    Template welcome;
+
+    // TODO: Add TemplateInstance
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public TemplateInstance get() {
+        return welcome.data("active", "home");
+    }
+}
